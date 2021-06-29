44e1d2c5880ace6e1886021f9f8b2aa141066f0e
Ling Vu <vumanhling@icloud.com>
2021-06-29 21:50:42 +0200

    Add header

---
+++ src/components/Header.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import useScrollTrigger from '@material-ui/core/useScrollTrigger';
+
+interface Props {
+  children: React.ReactElement;
+}
+
+function ElevationScroll(props: Props) {
+  const { children } = props;
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0,
+  });
+
+  return React.cloneElement(children, {
+    elevation: trigger ? 4 : 0,
+  });
+}
+
+export default function Header() {
+  return (
+    <ElevationScroll>
+      <AppBar>
+        <Toolbar>LingVuDev</Toolbar>
+      </AppBar>
+    </ElevationScroll>
+  );
+}
