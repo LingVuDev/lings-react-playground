d2ec98079c99961a4f7e462a179a2e2badb236b9
Ling Vu <vumanhling@icloud.com>
2021-06-30 22:21:25 +0200

    Add header items

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Button from '@material-ui/core/Button';
 import useScrollTrigger from '@material-ui/core/useScrollTrigger';
 import { makeStyles } from '@material-ui/core/styles';
 
@@ -24,16 +27,38 @@ const useStyles = makeStyles((theme) => ({
   toolbar: {
     ...theme.mixins.toolbar,
   },
+  tabContainer: { marginLeft: 'auto' },
+  button: {
+    borderRadius: 50,
+    margin: '0 25px',
+  },
 }));
 
 export default function Header() {
   const classes = useStyles();
+  const [value, setValue] = useState(0);
 
   return (
     <>
       <ElevationScroll>
         <AppBar>
-          <Toolbar>LingVuDev</Toolbar>
+          <Toolbar>
+            <span>LingVuDev</span>
+            <Tabs
+              className={`${classes.tabContainer} ${classes.toolbar}`}
+              value={value}
+              onChange={(e, value) => setValue(value)}
+            >
+              <Tab className={`${classes.toolbar}`} label="Home" />
+              <Tab className={`${classes.toolbar}`} label="Services" />
+              <Tab className={`${classes.toolbar}`} label="The Revolution" />
+              <Tab className={`${classes.toolbar}`} label="About us" />
+              <Tab className={`${classes.toolbar}`} label="Contact us" />
+            </Tabs>
+            <Button className={classes.button} variant="contained" color="secondary">
+              Free Estimate
+            </Button>
+          </Toolbar>
         </AppBar>
       </ElevationScroll>
       <div className={classes.toolbar}></div>
